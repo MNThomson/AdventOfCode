19ec3986943dbdb53e38576429633d7aef552feb
Max Thomson <git@mnt.dev>
2022-12-02 14:32:40 -0800

    [2022:2:Py] Clean up solution

--- 2022/day2/python/1.py
+++ 2022/day2/python/1.py
@@ -5,24 +5,40 @@ A: Rock,
 B: Paper,
 C: Scissors
 
+Pt 1:
 X: Rock,
-Y: Paper, 
+Y: Paper,
 Z: Scissors
+
+Pt 2:
+X: lose,
+Y: draw,
+Z: win
 """
 
-score = [
-    [4, 8, 3], 
-    [1, 5, 9], 
+part1_val = [
+    [4, 8, 3],
+    [1, 5, 9],
     [7, 2, 6]
 ]
 
+part2_val = [
+    [3, 4, 8],
+    [1, 5, 9],
+    [2, 6, 7]
+]
 
-with open("../input.txt") as my_file:
-    data = my_file.readlines()
+with open("../input.txt") as file:
+    data = file.readlines()
 
-    val = 0
+    part1, part2 = 0, 0
+
     for game in data:
         [elf, me] = game.split(" ", 2)
-        val += score[ord(elf) - 65][ord(me.rstrip()) - 88]
+        elf, me = ord(elf) - ord("A"), ord(me.rstrip()) - ord("X")
 
-    print(f"Part 1: {val}")
+        part1 += part1_val[elf][me]
+        part2 += part2_val[elf][me]
+
+    print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")
